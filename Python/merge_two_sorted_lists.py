7089f5d66193b373adaadda4c2f70aa12c04bb79
Nikita Malhotra <nmalhotra_24@hotmail.com>
2016-01-25 17:25:48 -0800

    merge_two_sorted_lists

---
+++ Python/merge_two_sorted_lists.py
@@ -0,0 +1,23 @@
+""" Merge two sorted linked lists and return it as a new list. The new list
+    should be made by splicing together the nodes of the first two lists.
+"""
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        if l1 is None:
+            return l2
+        if l2 is None:
+            return l1
+        if (l1.val < l2.val):
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l2.next, l1)
+            return l2
+        
