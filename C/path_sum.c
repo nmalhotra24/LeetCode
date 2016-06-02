2702adcb0ba276853920a03682c6216649349778
Nikita Malhotra <nmalhotra_24@hotmail.com>
2016-06-02 14:29:27 -0700

    path_sum

--- C/path_sum.c
+++ C/path_sum.c
@@ -21,7 +21,10 @@
  * };
  */
 bool hasPathSum(struct TreeNode* root, int sum) {
-  if (root == NULL) return false;
-  if ((root->left == NULL) && (root->right == NULL) && (root->val == sum)) return true;
-  return (hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->right));
+    if (root == NULL) return false;
+    int addition = sum - root->val;
+    if (root->left == NULL && root->right == NULL) {
+        return addition == 0;
+    }
+    return hasPathSum(root->left, addition) || hasPathSum(root->right, addition);
 }
